e7dec8353c8921c960b91e562147088812408591
Cronay <inf@yannic-borgfeld.de>
2020-08-28 18:28:48 +0200

    Formatted test to follow the convention of 'Prepare -> Act -> Assert'

--- Tests/LAnimationGenTests/CodeGeneratorTests.swift
+++ Tests/LAnimationGenTests/CodeGeneratorTests.swift
@@ -21,14 +21,18 @@ class CodeGeneratorTests: XCTestCase {
 
     func test_generateClassWithoutFile() {
         let codeGenerator = CodeGenerator()
+
         let code = codeGenerator.generate(fileList: [])
+
         XCTAssertEqual(codeStart + codeEnd, code)
     }
 
     func test_generateClassWithOneFile() {
         let codeGenerator = CodeGenerator()
-        let generatedFileAccessor = "    internal static let a = Animation.named(\"a\")"
+
         let code = codeGenerator.generate(fileList: ["a"])
+
+        let generatedFileAccessor = "    internal static let a = Animation.named(\"a\")"
         let expectedOutcome = codeStart + "\n"
             + generatedFileAccessor + "\n"
             + codeEnd
@@ -43,14 +47,11 @@ class CodeGeneratorTests: XCTestCase {
         let firstGeneratedLine = "    internal static let a = Animation.named(\"a\")"
         let secondGeneratedLine = "    internal static let b = Animation.named(\"b\")"
         let thirdGeneratedLine = "    internal static let c = Animation.named(\"c\")"
-
         let expectedOutcome = codeStart + "\n"
             + firstGeneratedLine + "\n"
             + secondGeneratedLine + "\n"
             + thirdGeneratedLine + "\n"
             + codeEnd
-
-
         XCTAssertEqual(expectedOutcome, code)
     }
 }
