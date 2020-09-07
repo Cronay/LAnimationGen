b4298e3dbbc856205a67058dcd634207aacc58a0
Cronay <inf@yannic-borgfeld.de>
2020-09-07 22:29:57 +0200

    Reordered tests and combined them

--- Tests/LAnimationGenTests/NameFormatterTests.swift
+++ Tests/LAnimationGenTests/NameFormatterTests.swift
@@ -60,22 +60,6 @@ private extension String {
 
 class NameFormatterTests: XCTestCase {
 
-    func test_format_doesAlterAlreadyFormattedNames() {
-        let alreadyFormattedNames = ["aNameWithCamelCase", "anamewithoutanyseparation", "aNAMEWITHALLCAPITALBUTFIRST"]
-
-        let newlyFormattedNames = NameFormatter.format(alreadyFormattedNames)
-
-        XCTAssertEqual(alreadyFormattedNames, newlyFormattedNames)
-    }
-
-    func test_format_nameWithAnUnderScoreIsGivenCamelCaseWithoutUnderscore() {
-        let nameWithUnderScore = "a_name"
-
-        let formattedName = NameFormatter.format([nameWithUnderScore])
-
-        XCTAssertEqual(["aName"], formattedName)
-    }
-
     func test_format_emptyListOfNamesReturnsEmptyList() {
         let formattedName = NameFormatter.format([])
 
@@ -86,18 +70,6 @@ class NameFormatterTests: XCTestCase {
         let formattedName = NameFormatter.format([""])
 
         XCTAssertEqual([""], formattedName)
-    }
-
-    func test_format_nameWithAnUnderScoreAtTheBeginning() {
-        let formattedName = NameFormatter.format(["_name"])
-
-        XCTAssertEqual(["name"], formattedName)
-    }
-
-    func test_format_nameWithAnUnderScoreAtTheEnd() {
-        let formattedName = NameFormatter.format(["name_"])
-
-        XCTAssertEqual(["name"], formattedName)
     }
 
     func test_format_capitalizedNameIsNotCapitalizedAfterwards() {
@@ -112,10 +84,19 @@ class NameFormatterTests: XCTestCase {
         XCTAssertEqual(["name"], formattedName)
     }
 
-    func test_format_nameWithDashInFront() {
-        let formattedName = NameFormatter.format(["-name"])
+    func test_format_doesAlterAlreadyFormattedNames() {
+        let alreadyFormattedNames = ["aNameWithCamelCase", "anamewithoutanyseparation", "aNAMEWITHALLCAPITALBUTFIRST"]
 
-        XCTAssertEqual(["name"], formattedName)
+        let newlyFormattedNames = NameFormatter.format(alreadyFormattedNames)
+
+        XCTAssertEqual(alreadyFormattedNames, newlyFormattedNames)
     }
 
+    func test_format_underScoreCasesRepresentingAllOtherSymbols() {
+        let namesWithUnderScore = ["a_name", "_name", "name_", "a_name_is_a_name", "a__name"]
+
+        let formattedName = NameFormatter.format(namesWithUnderScore)
+
+        XCTAssertEqual(["aName", "name", "name", "aNameIsAName", "aName"], formattedName)
+    }
 }
